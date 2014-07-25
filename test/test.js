38f05f60e948ac4011365a49ab1b8ae96f9fdc5a
Joel Simon <joelsimon6@gmail.com>
2014-07-25 10:37:20 -0700

    fixed whitespace

--- test/test.js
+++ test/test.js
@@ -15,8 +15,8 @@ describe('node-replay', function(){
       assert.throws(Replay, Error)
     });
     it('checks clientId', function(){
-    	var noClientId = function(){
-      	Replay('replayKey')
+      var noClientId = function(){
+        Replay('replayKey')
       }
       assert.throws(noClientId, Error)
     });
@@ -30,76 +30,76 @@ describe('node-replay', function(){
     });
 
     it('sets the options', function(){
-    	var options = {
-    		debug: true,
-    		sendAt: 42,
-  			sendAfter: 12000,
-  			debug: false,
-  			host: 'http://myhost.io'
-    	}
-    	var replay = Replay('Replay', 'DistinctId', options);
-    	for (var key in options) {
-    		expect(replay[key] = options[key])
-    	}
+      var options = {
+        debug: true,
+        sendAt: 42,
+        sendAfter: 12000,
+        debug: false,
+        host: 'http://myhost.io'
+      }
+      var replay = Replay('Replay', 'DistinctId', options);
+      for (var key in options) {
+        expect(replay[key] = options[key])
+      }
     });
   });
 
   context('tracking', function(){
-  	beforeEach(function() {
-  		var options = {host: 'http://localhost:3003'};
-  		this.replay = Replay('Replay','DistinctId', options);
+    beforeEach(function() {
+      var options = {host: 'http://localhost:3003'};
+      this.replay = Replay('Replay','DistinctId', options);
     });
 
-  	it('queues events', function(){
-			var props = {foo : 42};
-  		this.replay.sendAt = 3;
-  		this.replay.event('eventA', props);
-  		console.log(this.replay.queue[0]);
-  		expect(this.replay.queue[0]).to.deep.equal({
-  			type: 'event',
-  			request: { event_name: 'eventA', properties: props }
-  		});
-  	});
+    it('queues events', function(){
+      var props = {foo : 42};
+      this.replay.sendAt = 3;
+      this.replay.event('eventA', props);
+      console.log(this.replay.queue[0]);
+      expect(this.replay.queue[0]).to.deep.equal({
+        type: 'event',
+        request: { event_name: 'eventA', properties: props }
+      });
+    });
 
-  	it('queues traits', function(){
-			var props = {foo : 42};
-  		this.replay.sendAt = 3;
-  		this.replay.trait('traitA', props);
-  		expect(this.replay.queue[0]).to.deep.equal({
-  			type: 'trait',
-  			request: { properties: props }
-  		});
-  	});
+    it('queues traits', function(){
+      var props = {foo : 42};
+      this.replay.sendAt = 3;
+      this.replay.trait('traitA', props);
+      expect(this.replay.queue[0]).to.deep.equal({
+        type: 'trait',
+        request: { properties: props }
+      });
+    });
 
-  	it('does not queue events with sendAt=1', function(){
-			var myEvent = {foo : 42};
-  		this.replay.sendAt = 1;
-  		this.replay.event('eventA',myEvent);
-  		expect(this.replay.queue.length).to.equal(0);
-  	});
+    it('does not queue events with sendAt=1', function(){
+      var myEvent = {foo : 42};
+      this.replay.sendAt = 1;
+      this.replay.event('eventA',myEvent);
+      expect(this.replay.queue.length).to.equal(0);
+    });
 
-  	it('queues the appropriate number', function(){
-  		var myEvent = {foo : 42};
-  		this.replay.sendAt = 3;
-  		this.replay.event('eventA',myEvent);
-  		this.replay.event('eventB',myEvent);
-  		expect(this.replay.queue.length).to.equal(2);
-  		this.replay.event('eventC',myEvent);
-  		expect(this.replay.queue.length).to.equal(0);
-  	});
+    it('queues the appropriate number', function(){
+      var myEvent = {foo : 42};
+      this.replay.sendAt = 3;
+      this.replay.event('eventA',myEvent);
+      this.replay.event('eventB',myEvent);
+      expect(this.replay.queue.length).to.equal(2);
+      this.replay.event('eventC',myEvent);
+      expect(this.replay.queue.length).to.equal(0);
+    });
 
     it('sets the options', function(){
-    	var options = {
-    		debug: true,
-    		sendAt: 42,
-  			sendAfter: 12000,
-  			debug: false,
-  			host: 'http://myhost.io'
-    	}
-    	var replay = Replay('Replay', 'DistinctId', options);
-    	for (var key in options) {
-    		expect(replay[key] = options[key])
-    	}
+      var options = {
+        debug: true,
+        sendAt: 42,
+        sendAfter: 12000,
+        debug: false,
+        host: 'http://myhost.io'
+      }
+      var replay = Replay('Replay', 'DistinctId', options);
+      for (var key in options) {
+        expect(replay[key] = options[key])
+      }
     });
   });
 
